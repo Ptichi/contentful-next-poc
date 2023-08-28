15d1d4723fe0590fffbdf2484f758bf43d4d91d1
idan.ptichi <idan.ptichi@naturalint.com>
2023-08-28 23:32:08 +0300

    fix: type issue

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -11,7 +11,7 @@ export async function GET(request: Request) {
     return new Response('Invalid token', { status: 401 })
   }
 
-  const post = await getPreviewPostBySlug(slug)
+  const post = await getPreviewPostBySlug(slug as string)
 
   if (!post) {
     return new Response('Invalid slug', { status: 401 })
